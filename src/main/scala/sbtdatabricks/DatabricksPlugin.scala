ca0c917f4b681cb3559ba3a98f8a56db1397e5b1
Burak Yavuz <brkyvz@gmail.com>
2015-06-10 11:23:14 -0700

    added scalastyle

--- src/main/scala/sbtdatabricks/DatabricksPlugin.scala
+++ src/main/scala/sbtdatabricks/DatabricksPlugin.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 Databricks
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package sbtdatabricks
 
 import sbt._
@@ -184,7 +200,7 @@ object DatabricksPlugin extends AutoPlugin {
   }
 
   val baseDBCSettings: Seq[Setting[_]] = Seq(
-    dbcUsername := { 
+    dbcUsername := {
       sys.error(
         """
           |dbcUsername not defined. Please make sure to add these keys to your build:
@@ -194,7 +210,7 @@ object DatabricksPlugin extends AutoPlugin {
           |  See the sbt-databricks README for more info.
         """.stripMargin)
     },
-    dbcPassword := { 
+    dbcPassword := {
       sys.error(
         """
           |dbcPassword not defined. Please make sure to add these keys to your build:
@@ -204,7 +220,7 @@ object DatabricksPlugin extends AutoPlugin {
           |  See the sbt-databricks README for more info.
         """.stripMargin)
     },
-    dbcApiUrl := { 
+    dbcApiUrl := {
       sys.error(
         """
           |dbcApiUrl not defined. Please make sure to add these keys to your build:
